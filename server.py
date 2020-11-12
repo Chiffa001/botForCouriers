203e8daf2285ba7036bb74a7922a04c7a0d509d2
Anton Nakonechny <nakonechny.anton.js@gmail.com>
2020-11-12 14:25:50 +0300

    init: initial commit

---
+++ server.py
@@ -0,0 +1,22 @@
+import logging
+from config import API_TOKEN, ACCESS_ID
+
+from aiogram import Bot, Dispatcher, executor, types
+
+from middlewares import AccessMiddleware
+
+logging.basicConfig(level=logging.INFO)
+
+
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
+dp.middleware.setup(AccessMiddleware(ACCESS_ID))
+
+
+@dp.message_handler(commands=['start', 'help'])
+async def send_welcome(message: types.message):
+    print(message)
+    await message.answer("Бот для курьеров")
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
