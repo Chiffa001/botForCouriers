a905e7e981b57faab0b2b95a55b7a648fe0e3665
Anton Nakonechny <nakonechny.anton.js@gmail.com>
2020-11-12 15:15:17 +0300

    feat: add sql request for create a database

---
+++ createdb.sql
@@ -0,0 +1,28 @@
+create table daily_rate(
+    id integer primary key increment,
+    rate float
+);
+
+create table payment_method(
+    codename varchar(255) primary key,
+    name varchar(255),
+    aliases text
+);
+
+create table delivery(
+    id integer primary key,
+    amount float,
+    created datetime,
+    payment_codename varchar(255),
+    rate_id integer,
+    raw_text: text,
+    FOREIGN KEY(payment_codename) REFERENCES payment_method(codename),
+    FOREIGN KEY(rate) REFERENCES daily_rate(id)
+);
+
+insert into daily_rate (rate) value (3.6), (3.1), (3.3), (3.9), (2.8);
+insert into payment_method (codename, name, aliases)
+values
+    ("cash", "наличные", "нал, налик"),
+    ("non-cash", "безналичные", "без, безнал, безнальчик"),
+    ("paid", "оплачен", "опл, ничего, ниче, оплачено");
