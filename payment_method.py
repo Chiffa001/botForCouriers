3bbd19bbf35ffff871f2b9a028217370209ba0f2
Anton Nakonechny <nakonechny.anton.js@gmail.com>
2020-11-14 17:52:44 +0300

    feat: add handler for payment methods

--- payment_method.py
+++ payment_method.py
@@ -1,4 +1,4 @@
-from typing import NamedTuple, List
+from typing import NamedTuple, List, Dict
 
 import db
 
@@ -8,6 +8,9 @@ class PaymentMethod(NamedTuple):
     name: str
     aliases: List[str]
 
+    def __str__(self) -> str:
+        return f"Способ оплаты: {self.name},\nВозможно указывать в виде: {self.aliases}"
+
 
 class PaymentMethods:
 
@@ -15,10 +18,24 @@ class PaymentMethods:
         self._payment_methods = self._load_payment_methods()
 
     @staticmethod
-    def _load_payment_methods():
+    def _load_payment_methods() -> List[PaymentMethod]:
         payment_methods = db.fetch_all("payment_method", "codename name aliases".split())
-        print(payment_methods)
+        payment_methods = PaymentMethods._fill_aliases(payment_methods)
         return payment_methods
 
-    def get_all_payment_methods(self):
+    def get_all_payment_methods(self) -> List[PaymentMethod]:
         return self._payment_methods
+
+    @staticmethod
+    def _fill_aliases(payment_methods: List[Dict]) -> List[PaymentMethod]:
+        payment_methods_result = []
+        for payment_method in payment_methods:
+            aliases = payment_method['aliases'].split(",")
+            aliases = list(map(str.strip, aliases))
+
+            payment_methods_result.append(PaymentMethod(
+                codename=payment_method.get("codename"),
+                name=payment_method.get("name"),
+                aliases=aliases
+            ))
+        return payment_methods_result
--- server.py
+++ server.py
@@ -16,8 +16,16 @@ dp.middleware.setup(AccessMiddleware(ACCESS_ID))
 
 @dp.message_handler(commands=['start', 'help'])
 async def send_welcome(message: types.message):
-    PaymentMethods()
     await message.answer("Бот для курьеров")
+
+
+@dp.message_handler(commands=['payment'])
+async def get_payment_methods(message: types.Message):
+    payment_methods = PaymentMethods().get_all_payment_methods()
+    answer_message = ""
+    for string in [str(payment_method) for payment_method in payment_methods]:
+        answer_message += f"{string}\n\n"
+    await message.answer(answer_message)
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
